7cbb73805bd145d4a2c1bd14deca422debf74927
GreyA <grey.altaer@gmail.com>
2025-03-11 13:38:39 -0700

    build: IngredientsList.jsx

--- src/components/BurgerStack/BurgetStack.jsx
+++ src/components/BurgerStack/BurgetStack.jsx
@@ -1,5 +1,13 @@
-const BurgerStack = (props) => {
-  return <ul>// map through props.ingredients</ul>;
+const BurgerStack = (stack) => {
+  return (
+  <ul>
+    {stack.map((ingredient, index) => {
+        <li key={index}>
+            {ingredient.name}
+        </li>
+    })}
+  </ul>
+  );
 };
 
 export default BurgerStack;
--- src/components/IngredientList/IngredientList.jsx
+++ src/components/IngredientList/IngredientList.jsx
@@ -1,5 +1,14 @@
-const IngredientList = (props) => {
-  return <ul>// map through props.ingredients</ul>;
+const IngredientList = ({ingredients, addedIngredient}) => {
+  return (
+  <ul>
+    {ingredients.map((ingredient, index) => {
+        <li key={index}>
+            {ingredient.name}
+            <button onClick={() => addedIngredient(ingredient)}>Add Ingredient</button>
+        </li>
+    })}
+  </ul>
+  );
 };
 
 export default IngredientList;
